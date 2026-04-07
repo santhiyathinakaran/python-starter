63f94b25336af6411f60c151dc1765088f4e6cb2
santhiyathinakaran <santhiyathinakaran28@gmail.com>
2026-04-07 21:57:59 +0530

    Add files via upload

---
+++ my-python-starter.py
@@ -0,0 +1,16 @@
+name=input("enter your name:")
+a=int(input("enter your mark:"))
+b=int(input("enter your mark:"))        
+c=int(input("enter your mark:"))
+d=int(input("enter your mark:"))
+Total=a+b+c+d
+Average=Total/4
+print("name:",name)
+print("Total:",Total)
+print("Average:",Average)
+if Average>50:
+    print("pass")
+else:
+    print("fail")
+              
+
