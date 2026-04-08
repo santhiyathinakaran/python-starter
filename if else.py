ddf3ca699d1cea0ecfcc5bf6dde467f25c623d08
santhiyathinakaran <santhiyathinakaran28@gmail.com>
2026-04-08 21:25:12 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,17 @@
+mark=int(input())
+if mark < 0 or mark >100:
+    print("invalid marks")
+elif mark>=90:
+    print("Grade A")
+    print("Execellent")
+elif mark>=80:
+    print("Grade B")
+elif mark>=70:
+    print("Grade C")
+elif mark>=35:
+    print("Grade D")
+else:
+    print("Fail")
+    
+    
+    
